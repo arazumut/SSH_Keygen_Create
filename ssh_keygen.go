1a7f1974a589f9c73fdc6dae4cf108e349fa8baf
Umut <arazumut349@gmail.com>
2025-02-06 16:04:25 +0300

    Refactor SSH key generation and improve error handling

--- ssh_keygen.go
+++ ssh_keygen.go
@@ -1,92 +1,69 @@
 package main
 
 import (
-    "crypto/rand"
-    "crypto/rsa"
-    "crypto/x509"
-    "encoding/pem"
-    "fmt"
-    "os"
-    "os/signal"
-    "syscall"
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/pem"
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
 
-    "golang.org/x/crypto/ssh"
+	"golang.org/x/crypto/ssh"
 )
 
 func main() {
-    // Kanal oluşturma ve sinyal yakalama
-    sigs := make(chan os.Signal, 1)
-    signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	// Kanal oluşturma ve sinyal yakalama
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
 
-    done := make(chan bool, 1)
+	done := make(chan bool, 1)
 
-    go func() {
-        <-sigs
-        fmt.Println("\nSinyal yakalandı, program sonlandırılıyor...")
-        done <- true
-    }()
+	go func() {
+		<-sigs
+		fmt.Println("\nSinyal yakalandı, program sonlandırılıyor...")
+		done <- true
+	}()
 
-    fmt.Println("SSH anahtarları oluşturuluyor...")
+	fmt.Println("SSH anahtarları oluşturuluyor...")
 
-    // Anahtar boyutu
-    keySize := 2048
+	// Anahtar boyutu
+	keySize := 2048
 
-    // Özel anahtar oluşturma
-    privateKey, err := rsa.GenerateKey(rand.Reader, keySize)
-    if err != nil {
-        fmt.Println("Özel anahtar oluşturulurken hata:", err)
-        return
-    }
+	// Özel anahtar oluşturma
+	privateKey, err := rsa.GenerateKey(rand.Reader, keySize)
+	if err != nil {
+		log.Fatalf("Özel anahtar oluşturulurken hata: %v", err)
+	}
 
-    // Özel anahtar dosyası oluşturma
-    privateKeyFile, err := os.Create("id_rsa")
-    if err != nil {
-        fmt.Println("Özel anahtar dosyası oluşturulamadı:", err)
-        return
-    }
-    
-    defer privateKeyFile.Close()
+	// Özel anahtarı PEM formatında kodlama
+	privateKeyPEM := pem.EncodeToMemory(&pem.Block{
+		Type:  "RSA PRIVATE KEY",
+		Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
+	})
 
-    // Özel anahtar PEM formatına çevirme ve dosyaya yazma
-    privateKeyPEM := pem.EncodeToMemory(
-        &pem.Block{
-            Type:  "RSA PRIVATE KEY",
-            Bytes: x509.MarshalPKCS1PrivateKey(privateKey),
-        },
-    )
-    _, err = privateKeyFile.Write(privateKeyPEM)
-    if err != nil {
-        fmt.Println("Özel anahtar dosyasına yazılamadı:", err)
-        return
-    }
+	// Özel anahtarı dosyaya yazma
+	err = os.WriteFile("id_rsa", privateKeyPEM, 0600)
+	if err != nil {
+		log.Fatalf("Özel anahtar dosyaya yazılırken hata: %v", err)
+	}
 
-    // Genel anahtar oluşturma
-    publicKey, err := ssh.NewPublicKey(&privateKey.PublicKey)
-    if err != nil {
-        fmt.Println("Genel anahtar oluşturulurken hata:", err)
-        return
-    }
+	// Genel anahtar oluşturma
+	publicKey, err := ssh.NewPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		log.Fatalf("Genel anahtar oluşturulurken hata: %v", err)
+	}
 
-    // Genel anahtar dosyası oluşturma
-    publicKeyFile, err := os.Create("id_rsa.pub")
-    if err != nil {
-        fmt.Println("Genel anahtar dosyası oluşturulamadı:", err)
-        return
-    }
-    defer publicKeyFile.Close()
+	// Genel anahtarı dosyaya yazma
+	err = os.WriteFile("id_rsa.pub", ssh.MarshalAuthorizedKey(publicKey), 0644)
+	if err != nil {
+		log.Fatalf("Genel anahtar dosyaya yazılırken hata: %v", err)
+	}
 
-    // Genel anahtarı dosyaya yazma
-    publicKeyBytes := ssh.MarshalAuthorizedKey(publicKey)
-    _, err = publicKeyFile.Write(publicKeyBytes)
-    if err != nil {
-        fmt.Println("Genel anahtar dosyasına yazılamadı:", err)
-        return
-    }
+	fmt.Println("SSH anahtarları başarıyla oluşturuldu ve kaydedildi.")
+	fmt.Println("Çıkmak için CTRL+C.")
 
-    fmt.Println("SSH anahtarları başarıyla oluşturuldu.")
-
-    // Programın sonlandırılmasını bekleme
-    fmt.Println("Çıkmak için Ctrl + C'ye basın...")
-    <-done
-    fmt.Println("Program kapatıldı.")
+	<-done
 }
